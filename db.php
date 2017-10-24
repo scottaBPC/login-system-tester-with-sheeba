d48a7468e47f1b317438f37678a91b45ffd35486
Alex Scott <scotta@bpc.ac.uk>
2017-10-24 13:36:29 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,14 +1,5 @@
-// PHP Data Objects(PDO) Sample Code:
-try {
-    $conn = new PDO("sqlsrv:server = tcp:sheebaserver.database.windows.net,1433; Database = testtablewithSheeba", "lynandal", "Lindy8656");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
-
-// SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "lynandal@sheebaserver", "pwd" => "Lindy8656", "Database" => "testtablewithSheeba", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-$serverName = "tcp:sheebaserver.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
+$host = 'tcp:sheebaserver.database.windows.net,1433';
+$user = 'lynandal@sheebaserver';
+$pass = 'Lindy8656';
+$db = 'accounts';
+$mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
