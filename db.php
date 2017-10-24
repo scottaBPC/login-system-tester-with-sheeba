2000e88264053471fab4c5265d88e20ba2ca1fad
Alex Scott <scotta@bpc.ac.uk>
2017-10-24 13:23:48 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,10 +1,6 @@
 <?php
 /* Database connection settings */
-$host = 'localhost';
-$user = 'root';
-$pass = 'mypass123';
-$db = 'accounts';
-$mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
+
 
 $connectstr_dbhost = '';
 $connectstr_dbname = '';
