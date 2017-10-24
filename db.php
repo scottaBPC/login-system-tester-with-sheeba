ad6af985ce1fde646c74d9fb3767780cd062a8c1
Alex Scott <scotta@bpc.ac.uk>
2017-10-24 13:32:47 +0100

    Update db.php

--- db.php
+++ db.php
@@ -18,7 +18,21 @@ foreach ($_SERVER as $key => $value) {
     $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
 }
 
-$link = mysqli_connect("tcp:sheebaserver.database.windows.net,1433", "lynandal@sheebaserver", "Lindy8656","testtablewithSheeba");
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:sheebaserver.database.windows.net,1433; Database = testtablewithSheeba", "lynandal", "Lindy8656");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "lynandal@sheebaserver", "pwd" => "Lindy8656", "Database" => "testtablewithSheeba", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:sheebaserver.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+
 
 if (!$link) {
     echo "Error: Unable to connect to MySQL." . PHP_EOL;
