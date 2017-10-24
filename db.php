0202ae06bf43a709de1e86ac29f93d38692e82b0
Alex Scott <scotta@bpc.ac.uk>
2017-10-24 13:34:17 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,26 +1,6 @@
-<?php
-/* Database connection settings */
-
-
-$connectstr_dbhost = '';
-$connectstr_dbname = '';
-$connectstr_dbusername = '';
-$connectstr_dbpassword = '';
-
-foreach ($_SERVER as $key => $value) {
-    if (strpos($key, "MYSQLCONNSTR_localdb") !== 0) {
-        continue;
-    }
-    
-    $connectstr_dbhost = preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbname = preg_replace("/^.*Database=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbusername = preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $value);
-    $connectstr_dbpassword = preg_replace("/^.*Password=(.+?)$/", "\\1", $value);
-}
-
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:sheebaserver.database.windows.net,1433; Database = testtablewithSheeba", "lynandal", "Lindy8656");
+    $conn = new PDO("sqlsrv:server = tcp:sheebaserver.database.windows.net,1433; Database = testtablewithSheeba", "lynandal", "{your_password_here}");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
@@ -29,19 +9,6 @@ catch (PDOException $e) {
 }
 
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "lynandal@sheebaserver", "pwd" => "Lindy8656", "Database" => "testtablewithSheeba", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$connectionInfo = array("UID" => "lynandal@sheebaserver", "pwd" => "{your_password_here}", "Database" => "testtablewithSheeba", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:sheebaserver.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
-
-
-if (!$link) {
-    echo "Error: Unable to connect to MySQL." . PHP_EOL;
-    echo "Debugging errno: " . mysqli_connect_errno() . PHP_EOL;
-    echo "Debugging error: " . mysqli_connect_error() . PHP_EOL;
-    exit;
-}
-
-echo "Success: A proper connection to MySQL was made! The my_db database is great." . PHP_EOL;
-echo "Host information: " . mysqli_get_host_info($link) . PHP_EOL;
-
-mysqli_close($link);
