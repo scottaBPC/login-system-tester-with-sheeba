81743f296c5f434eb947261824d4d1147b2e0467
Alex Scott <scotta@bpc.ac.uk>
2017-10-24 13:34:43 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,6 +1,6 @@
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:sheebaserver.database.windows.net,1433; Database = testtablewithSheeba", "lynandal", "{your_password_here}");
+    $conn = new PDO("sqlsrv:server = tcp:sheebaserver.database.windows.net,1433; Database = testtablewithSheeba", "lynandal", "Lindy8656");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
@@ -9,6 +9,6 @@ catch (PDOException $e) {
 }
 
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "lynandal@sheebaserver", "pwd" => "{your_password_here}", "Database" => "testtablewithSheeba", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$connectionInfo = array("UID" => "lynandal@sheebaserver", "pwd" => "Lindy8656", "Database" => "testtablewithSheeba", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:sheebaserver.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
