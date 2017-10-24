cc4018f6995a447290fd606ec9ae6ed31af21c48
Alex Scott <scotta@bpc.ac.uk>
2017-10-24 13:37:00 +0100

    Update db.php

--- db.php
+++ db.php
@@ -1,5 +1,5 @@
 $host = 'tcp:sheebaserver.database.windows.net,1433';
 $user = 'lynandal@sheebaserver';
 $pass = 'Lindy8656';
-$db = 'accounts';
+$db = 'testtablewithSheeba\accounts';
 $mysqli = new mysqli($host,$user,$pass,$db) or die($mysqli->error);
